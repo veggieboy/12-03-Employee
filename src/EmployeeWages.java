c24b60417e3dc69961ccb37d402967352b19c9bd
Don Irwin <don@falconwebtech.com>
2014-12-02 19:45:32 -0700

    Create EmployeeWages.java

---
+++ src/EmployeeWages.java
@@ -0,0 +1,33 @@
+// placeholder for your main method
+public class EmployeeWages {
+    public static void main(String[] args) {
+        System.out.println("Student: <put your name here>");
+        
+        // Create two instances of class Employee emp1 & emp2
+        
+        // print emp1 
+        
+        // print emp1's weekly salary (put a tab first) like "       weekly salary: $360.00" 
+        
+        // print emp1's annual salary (put a tab first) like "       annual salary: $18,720.00" 
+        
+        // print emp2
+        
+        // change emp2's name
+        
+        // change emp2's wage
+        
+        // change emp2's hours
+        
+        // print emp2 
+        
+        // print emp2's weekly salary (put a tab first) like "       weekly salary: $360.00" 
+        
+        // print emp2's annual salary (put a tab first) like "       annual salary: $18,720.00" 
+        
+        // give emp2 a raise
+        
+        // print emp2
+        
+    }
+}
